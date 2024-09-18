5ae9eedc0f0e7cc1217c4a3d7aaff8773f129e8b
Ashutosh Kumar <ashutoshkumarranchi1@gmail.com>
2024-09-18 19:32:34 +0530

    Updated sendMessageUpdate method
    Change Log:
    1. Logging and error handling functionalities were added
    2. sendMessageUpdate method now only has single responsibility of
       sending a message to the user. Previously it was handling creation of
       message that needs to be sent as well
    3. sendVideoUpdate method now receives an additional argument named
       outputDir: Path of the video generated

--- src/main/java/io/ashutosh/Telegram.java
+++ src/main/java/io/ashutosh/Telegram.java
@@ -9,6 +9,8 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,11 +23,12 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class Telegram {
+
+    static final Logger logger = LoggerFactory.getLogger(Telegram.class);
+
     final String urlString;
     final String telegramReceiverID;
     final ObjectMapper objectMapper;
@@ -36,73 +39,72 @@ public class Telegram {
         this.objectMapper = new ObjectMapper();
     }
 
-    private boolean checkForSuccessfulRequest(String response) throws JsonProcessingException {
-        return this.objectMapper.readTree(response).get("ok").asBoolean();
-    }
-
-    public void sendMessageUpdate(List<String> copiedFiles) throws IOException, InterruptedException {
-        String methodName = "/sendMessage";
-
-        Map<String, String> jsonBody = new HashMap<>();
-        jsonBody.put("chat_id", this.telegramReceiverID);
-        jsonBody.put("parse_mode", "HTML");
-        jsonBody.put("protect_content", Boolean.toString(true));
-
-        StringBuilder text = new StringBuilder();
-        text.append("<b>Backup Successful</b> ✅\n\n");
-        if (!copiedFiles.isEmpty()) {
-            text.append("<b><i>List of files that were backed up in Hard Disk:</i></b>\n");
-            IntStream.range(0, copiedFiles.size()).forEach(idx -> text.append(idx + 1).append(". ").append(copiedFiles.get(idx)).append("\n"));
-        } else {
-            text.append("<b><i>All files are already up to date!</i></b>");
+    private boolean checkForSuccessfulRequest(String response) {
+        try {
+            return this.objectMapper.readTree(response).get("ok").asBoolean();
+        } catch (JsonProcessingException e) {
+            logger.error("Unable to determine whether request was successful. Marking it as a failed request", e);
+            return false;
         }
-        jsonBody.put("text", text.toString());
-
-        String requestBody = this.objectMapper
-                .writerWithDefaultPrettyPrinter()
-                .writeValueAsString(jsonBody);
-
-        HttpRequest request = HttpRequest.newBuilder(URI.create(this.urlString + methodName))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                .build();
-
-        HttpResponse<String> response = HttpClient.newBuilder()
-                .build()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        if(checkForSuccessfulRequest(response.body()))
-            System.out.println("Message containing the list of files that were backed up has been sent");
-        else
-            System.out.println("Unable to send message update");
     }
 
-    public void sendVideoUpdate(String videoFileName) throws IOException {
-        String methodName = "/sendVideo";
+    public void sendMessageUpdate(String message) {
+        try {
+            String methodName = "/sendMessage";
 
-        InputStream inputStream = new FileInputStream(Paths.get("").toAbsolutePath().resolve(videoFileName).toString());
-        HttpEntity entity = MultipartEntityBuilder.create()
-                .addTextBody("chat_id", this.telegramReceiverID)
-                .addTextBody("caption", "Video Recording of pCloud backup: <b>" +
-                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("E, d LLL YYYY HH:mm:ss")) + "</b>")
-                .addTextBody("parse_mode", "HTML")
-                .addTextBody("show_caption_above_media", Boolean.toString(true))
-                .addTextBody("protect_content", Boolean.toString(true))
-                .addBinaryBody("video", inputStream, ContentType.create("video/mp4"), videoFileName)
-                .build();
+            Map<String, String> jsonBody = new HashMap<>();
+            jsonBody.put("chat_id", this.telegramReceiverID);
+            jsonBody.put("parse_mode", "HTML");
+            jsonBody.put("protect_content", Boolean.toString(true));
+            jsonBody.put("text", message);
 
-        HttpPost httpPost = new HttpPost(this.urlString + methodName);
-        httpPost.setEntity(entity);
+            String requestBody = this.objectMapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(jsonBody);
 
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
-            String response = client.execute(httpPost, new BasicHttpClientResponseHandler());
-            if(checkForSuccessfulRequest(response))
-                System.out.println("Video recording of pCloud backup has been sent");
+            HttpRequest request = HttpRequest.newBuilder(URI.create(this.urlString + methodName))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (checkForSuccessfulRequest(response.body()))
+                logger.info("Message update related to backup has been sent");
             else
-                System.out.println("Unable to send video recording of pCloud backup");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+                logger.error("Unsuccessful Telegram Request. Response received: {}", response.body());
+        } catch (IOException | InterruptedException e) {
+            logger.error("Something went wrong while sending message update! Unable to send HTTP POST request.", e);
         }
+    }
 
+    public void sendVideoUpdate(String outputDir, String videoFileName) {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            String methodName = "/sendVideo";
+
+            InputStream inputStream = new FileInputStream(Paths.get(outputDir).toAbsolutePath().resolve(videoFileName).toString());
+            HttpEntity entity = MultipartEntityBuilder.create()
+                    .addTextBody("chat_id", this.telegramReceiverID)
+                    .addTextBody("caption", "Video Recording of pCloud backup: <b>" +
+                            LocalDateTime.now().format(DateTimeFormatter.ofPattern("E, d LLL YYYY HH:mm:ss")) + "</b>")
+                    .addTextBody("parse_mode", "HTML")
+                    .addTextBody("show_caption_above_media", Boolean.toString(true))
+                    .addTextBody("protect_content", Boolean.toString(true))
+                    .addBinaryBody("video", inputStream, ContentType.create("video/mp4"), videoFileName)
+                    .build();
+
+            HttpPost httpPost = new HttpPost(this.urlString + methodName);
+            httpPost.setEntity(entity);
+
+            String response = client.execute(httpPost, new BasicHttpClientResponseHandler());
+            if (checkForSuccessfulRequest(response))
+                logger.info("Video recording of pCloud backup has been sent");
+            else
+                logger.error("Unsuccessful Telegram Request while sending video of pCloud backup! Response Received: {}", response);
+        } catch (IOException e) {
+            logger.error("Something went wrong while sending video update! Unable to send HTTP POST request.", e);
+        }
     }
 }
