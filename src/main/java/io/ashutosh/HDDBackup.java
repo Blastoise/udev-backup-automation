a00f455a688ecca03c3979df23414ae1ba0775f1
Ashutosh Kumar <ashutoshkumarranchi1@gmail.com>
2024-09-18 18:37:10 +0530

    Added logging and handled exceptions
    Change Log:
    1. Added logging using slf4j with logback
    2. Used try-with-resource to ensure stream opened by Files.walk gets
       closed in getSrcPathFileList method
    3. Added exceptional handling functionalities
    4. Removed creation of srcBasePath and destBasePath directories logic
       from constructor to backupAssets method. Now if srcBasePath does not exists it will throw
       RuntimeException to shutdown the application. Application tries to
       create destBasePath if it is not able to create it then HDDBack is skipped all together
    5. getSrcPathFileList method will throw RuntimeException if it encounters any
       issue thus skipping the backup of the whole path
    6. Similar approach is taken in copyNewOrAlteredFiles method where any
       issue while backup will lead to abrupt stopping of the path it was
       backing up.
    7. copyNewOrAlteredFiles now supports assets with nested files/directories
    8. backupAssets method now return a string representing a message that
       can be used to send an update to the user.

--- src/main/java/io/ashutosh/HDDBackup.java
+++ src/main/java/io/ashutosh/HDDBackup.java
@@ -1,41 +1,66 @@
 package io.ashutosh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static io.ashutosh.Utils.fileTimeComparator;
 
 public class HDDBackup {
 
+    private static final Logger logger = LoggerFactory.getLogger(HDDBackup.class);
+
     private final Path srcBasePath;
     private final Path destBasePath;
 
-    public HDDBackup(String srcBasePath, String destBasePath) throws IOException {
+    public HDDBackup(String srcBasePath, String destBasePath) {
         this.srcBasePath = Paths.get(srcBasePath);
-        Files.createDirectories(this.srcBasePath);
         this.destBasePath = Paths.get(destBasePath);
-        Files.createDirectories(this.destBasePath);
     }
 
-    private List<FileInfo> getSrcPathFileList(Path path) throws IOException {
-        return Files.walk(path, Integer.MAX_VALUE).map(filePath -> {
-            try {
-                return new FileInfo(this.srcBasePath.relativize(filePath).toString(), Files.readAttributes(filePath, BasicFileAttributes.class));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).collect(Collectors.toList());
+    private List<FileInfo> getSrcPathFileList(Path path) {
+        try (Stream<Path> files = Files.walk(path, Integer.MAX_VALUE)) {
+            return files.map(filePath -> {
+                try {
+                    return new FileInfo(this.srcBasePath.relativize(filePath).toString(), Files.readAttributes(filePath, BasicFileAttributes.class));
+                } catch (IOException e) {
+                    // Unlikely that we'll get an exception while reading attributes
+                    // But if we get we'll skip the whole path itself by throwing RuntimeException
+                    logger.error("Unable to read file attributes for file/folder: {}", filePath, e);
+                    throw new RuntimeException(e);
+                }
+            }).collect(Collectors.toList());
+        } catch (IOException e) {
+            logger.error("Unable to walk the file tree for the path: {}", path.toString(), e);
+            throw new RuntimeException(e);
+        }
     }
 
-    private void copyNewOrAlteredFiles(Path path, List<String> copiedFiles) throws IOException {
+    private void copyNewOrAlteredFiles(Path path, List<String> copiedFiles) {
+
         List<FileInfo> fileInfoList = getSrcPathFileList(path);
+
+        // For nested files/directories
+        // Parent directories might not be present while copying so creating necessary directories
+        Path parent = this.srcBasePath.relativize(path).getParent();
+        try {
+            if (parent != null) {
+                Files.createDirectories(this.destBasePath.resolve(parent));
+                logger.info("Created parent directory at destBasePath for the following path: {}", path);
+            }
+        } catch (IOException e) {
+            logger.error("Unable to create parent directory {}", parent, e);
+            throw new RuntimeException(e);
+        }
 
         for (FileInfo fileInfo : fileInfoList) {
             Path srcFilePath = this.srcBasePath.resolve(fileInfo.fileName);
@@ -43,24 +68,61 @@ public class HDDBackup {
             if (Files.exists(destFilePath)) {
                 // if file exists then compare last modified time and size
                 if (fileInfo.basicFileAttr.isRegularFile()) {
-                    BasicFileAttributes destFileAttr = Files.readAttributes(destFilePath, BasicFileAttributes.class);
+                    try {
+                        BasicFileAttributes destFileAttr = Files.readAttributes(destFilePath, BasicFileAttributes.class);
 
-                    if (fileTimeComparator(destFileAttr.lastModifiedTime(), fileInfo.basicFileAttr.lastModifiedTime()) || destFileAttr.size() != fileInfo.basicFileAttr.size()) {
-                        Files.copy(srcFilePath, destFilePath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-                        copiedFiles.add(fileInfo.fileName);
+                        if (fileTimeComparator(destFileAttr.lastModifiedTime(), fileInfo.basicFileAttr.lastModifiedTime()) || destFileAttr.size() != fileInfo.basicFileAttr.size()) {
+                            Files.copy(srcFilePath, destFilePath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+                            logger.info("File successfully replaced: {}", destFilePath);
+                            copiedFiles.add(fileInfo.fileName);
+                        } else logger.info("File exists and is up to date: {}", destFilePath);
+
+                    } catch (IOException e) {
+                        logger.error("IOException occurred while reading attributes or while copying for file: {}", destFilePath, e);
+                        throw new RuntimeException(e);
                     }
-                }
+                } else logger.info("Folder already exists: {}", destFilePath);
+
             } else {
                 // If file or folder does not exist then copy that file or copy empty folder
-                Files.copy(srcFilePath, destFilePath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-                if (fileInfo.basicFileAttr.isRegularFile()) copiedFiles.add(fileInfo.fileName);
+                try {
+                    Files.copy(srcFilePath, destFilePath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+                    logger.info("File/Folder successfully created: {}", destFilePath);
+                    if (fileInfo.basicFileAttr.isRegularFile()) copiedFiles.add(fileInfo.fileName);
+                } catch (IOException e) {
+                    logger.error("Unable to create file/folder with the following path: {}", destFilePath, e);
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
 
-    public List<String> backupAssets(String[] assets) throws IOException {
-        List<String> copiedFiles = new ArrayList<>();
-        for (String asset : assets) copyNewOrAlteredFiles(this.srcBasePath.resolve(asset), copiedFiles);
-        return copiedFiles;
+    public String backupAssets(String[] assets, List<String> copiedFiles, List<String> failedPaths) {
+        try {
+            if (!Files.exists(this.srcBasePath)) {
+                logger.error("srcBasePath does not exists");
+                throw new RuntimeException("srcBasePath does not exist");
+            }
+
+            Files.createDirectories(this.destBasePath);
+
+            for (String asset : assets) {
+                try {
+                    copyNewOrAlteredFiles(this.srcBasePath.resolve(asset), copiedFiles);
+                } catch (RuntimeException e) {
+                    failedPaths.add(this.srcBasePath.resolve(asset).toString());
+                    logger.info("Gracefully handled RuntimeException for path: {}", this.srcBasePath.resolve(asset));
+                }
+            }
+
+            return Utils.messageTextBuilder(copiedFiles, failedPaths, failedPaths.size() == assets.length);
+
+        } catch (IOException e) {
+            String text = "<b>Backup Failed</b> ❌\n\n" +
+                    "<b><i>Error occurred while creating folder " + destBasePath + "</i></b>\n";
+            logger.error("Error while creating destBasePath", e);
+            logger.warn("Hard Disk backup skipped");
+            return text;
+        }
     }
 }
