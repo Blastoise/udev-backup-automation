cf58e582c925d65f8a6c84d9b5c7fab78dc14e09
Ashutosh Kumar <ashutoshkumarranchi1@gmail.com>
2024-09-18 18:52:40 +0530

    Refactored PCloudBackup.java and Utils.java
    Change Log:
    
    PCloudBackup.java:
    1. Used logback for logging instead of outputing to console using
       System.out.println
    2. Creating webdriver instance and configuring it is moved to setup
       method
    3. setup method also takes care of creating a session for using CDP,
       calling registering event listeners for getting frames and creating
       temporary directory where we will dump the frames
    4. screenRecording method now takes an argument tmpDir which dictates where
       frames recieved from Devtools will be saved
    5. Reduced the time to wait for the pop-up to appear which controls
       whether the file/folder needs to be replaced if already present
    6. Increased the wait time for uploadChecker so that large uploads can
       be done
    7. Added cleanUp method which will close the browser as well as devtool
       sessions
    8. Error paths were handled so that if any RuntimeException is thrown
       then browser session as well as frames if created are deleted
    
    Utils.java
    1. Logging and exception handling has been taken care of
    2. generateVideoFromFrames method now gets two additional arguments
       tmpDir: Path where frames are stored
       outputDir: Path where video recording needs to be stored
    3. deleteFrames method also gets two arguments
       frameNamePrefix: Prefix attached for naming frames
       tmpDir: Path where frames are stored
    4. Framerate from 15 has been increased to 30 so that length of video
       becomes short
    5. messageTextBuilder method has been added to generate a message that
       will be sent for status update of Hard Disk backup. The backup is
       considered successful if failedPaths(list of paths which threw a
       RuntimeException while backup for any reason) is empty. Backup is
       considered failed if failedPaths contains all of the paths that were
       send for backup. If not any of these it is considered partially
       successful

--- src/main/java/io/ashutosh/PCloudBackup.java
+++ src/main/java/io/ashutosh/PCloudBackup.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v128.page.Page;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
@@ -23,25 +25,21 @@ import java.util.function.BiPredicate;
 
 public class PCloudBackup {
 
+    private static final Logger logger = LoggerFactory.getLogger(PCloudBackup.class);
     private static PCloudBackup pCloudBackup = null;
-    private final WebDriver webDriver;
+    private WebDriver webDriver;
+    private DevTools devTools;
     private final Path srcBasePath;
     private final String username;
     private final String password;
+
+    // Prefix for the name of the frames
     private final String frameNamePrefix = "frame-";
+    // Count of frames saved
     private Long seqNum = 0L;
 
 
     private PCloudBackup(String srcBasePath, String username, String password) {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-        chromeOptions.addArguments("--headless=new");
-        chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addArguments("--disable-infobars");
-
-        this.webDriver = new ChromeDriver(chromeOptions);
-        this.webDriver.manage().window().maximize();
-
         this.srcBasePath = Paths.get(srcBasePath);
         this.username = username;
         this.password = password;
@@ -58,8 +56,8 @@ public class PCloudBackup {
             wait.until(driver -> execFn.test(driver, locator));
             return this.webDriver.findElement(locator);
         } catch (TimeoutException ex) {
-            System.out.println(ex.getMessage());
-            throw new TimeoutException("Time out waiting for element with locator: " + locator);
+            logger.error("Time out waiting for element with locator: {}", locator, ex);
+            throw ex;
         }
     }
 
@@ -69,42 +67,49 @@ public class PCloudBackup {
         for (String file : filePathList) sb.append(file).append("\n");
         sb.delete(sb.length() - 1, sb.length());
 
+        logger.info("Starting file uploads");
+
         WebElement uploadFile = waitFor(timeout, By.xpath("//input[@type='file' and @multiple='']"), Utils.elementPresent(false));
         uploadFile.sendKeys(sb.toString());
 
+
         // Replace all files in cloud regardless of timestamp of items(pop up appears instantly):
         try {
-            WebElement applyForAllCheckBox = waitFor(timeout, By.xpath("//div[contains(@class,'InputStyled') and contains(text(),'Apply for all')]"), Utils.elementPresent(true));
+            WebElement applyForAllCheckBox = waitFor(Duration.ofSeconds(10), By.xpath("//div[contains(@class,'InputStyled') and contains(text(),'Apply for all')]"), Utils.elementPresent(true));
             Utils.jsClick(this.webDriver, applyForAllCheckBox);
 
-            WebElement continueBtn = waitFor(timeout, By.xpath("//a[@color='cyan']/span[contains(text(),'Continue')]"), Utils.elementPresent(true));
+            WebElement continueBtn = waitFor(Duration.ofSeconds(10), By.xpath("//a[@color='cyan']/span[contains(text(),'Continue')]"), Utils.elementPresent(true));
             Utils.jsClick(this.webDriver, continueBtn);
 
         } catch (Exception e) {
-            System.out.println("All files are new");
+            logger.info("All files being uploaded are new and doesn't exist on pCloud");
         }
     }
 
     // uploads one folder at a time
     private void folderUpload(Duration timeout, String folderPath) {
+        logger.info("Starting folder upload for: {}", folderPath);
+
         WebElement uploadFile = waitFor(timeout, By.xpath("//input[@type='file' and @webkitdirectory='true']"), Utils.elementPresent(false));
         uploadFile.sendKeys(folderPath);
 
         // Replace folder in cloud regardless of last modified time of items:
         try {
-            WebElement continueBtn = waitFor(timeout, By.xpath("//a[@color='cyan']/span[contains(text(),'Continue')]"), Utils.elementPresent(true));
+            WebElement continueBtn = waitFor(Duration.ofSeconds(20), By.xpath("//a[@color='cyan']/span[contains(text(),'Continue')]"), Utils.elementPresent(true));
             Utils.jsClick(this.webDriver, continueBtn);
         } catch (Exception e) {
-            System.out.println("Folder is new");
+            logger.info("Folder: {} is new and doesn't exist on pCloud", folderPath);
         }
     }
 
     private void uploadChecker(Duration timeout, Long cntOfAssets) {
+        logger.info("Started monitoring completion of file/folder uploads");
+
         WebElement allItemsBtn = waitFor(timeout, By.xpath("//*[@id='upload-manager-container']//label[contains(text(),'All items')]"), Utils.elementPresent(true));
         Utils.jsClick(this.webDriver, allItemsBtn);
 
         waitFor(timeout, By.xpath("//*[@id='upload-manager-container']//p[contains(text(),'Completed')]/following-sibling::p"), Utils.uploadComplete(cntOfAssets));
-        System.out.println("Successfully uploaded files and folders!!!");
+        logger.info("Successfully uploaded files and folders!!!");
     }
 
     private void login() {
@@ -121,9 +126,11 @@ public class PCloudBackup {
 
         nxtButton = waitFor(Duration.ofSeconds(50), By.cssSelector("button.butt.submitbut"), Utils.elementPresent(true));
         Utils.jsClick(this.webDriver, nxtButton);
+
+        logger.info("Login Successful");
     }
 
-    private void screenRecording(DevTools devTools) {
+    private void screenRecording(DevTools devTools, String tmpDir) {
         devTools.send(Page.startScreencast(
                 Optional.empty(),
                 Optional.empty(),
@@ -136,22 +143,49 @@ public class PCloudBackup {
             byte[] frameImage = Base64.getDecoder().decode(screencastFrame.getData());
             try {
                 OutputStream out = new BufferedOutputStream(new FileOutputStream(
-                        frameNamePrefix + this.seqNum + ".png"));
+                        tmpDir + "/" + frameNamePrefix + this.seqNum + ".png"));
                 out.write(frameImage);
                 out.close();
                 this.seqNum++;
             } catch (IOException e) {
+                logger.error("Unable to save frames. Closing the backup process", e);
                 throw new RuntimeException(e);
             }
         });
     }
 
-    public void backupAssets(String[] assets, String videoFileName) throws InterruptedException, IOException {
-        try {
-            DevTools devTools = ((ChromeDriver) this.webDriver).getDevTools();
-            devTools.createSession();
+    private void setup(String tmpDir) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        chromeOptions.addArguments("--headless=new");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--disable-infobars");
 
-            this.screenRecording(devTools);
+        this.webDriver = new ChromeDriver(chromeOptions);
+        this.webDriver.manage().window().maximize();
+
+        this.devTools = ((ChromeDriver) this.webDriver).getDevTools();
+        this.devTools.createSession();
+
+        // Create a folder for storing frames
+        try {
+            Files.createDirectories(Paths.get(tmpDir));
+        } catch (IOException e) {
+            logger.error("Unable to create temporary directory: {}", tmpDir, e);
+            throw new RuntimeException(e);
+        }
+        this.screenRecording(this.devTools, tmpDir);
+    }
+
+    private void cleanUp() {
+        devTools.disconnectSession();
+        devTools.close();
+        this.webDriver.quit();
+    }
+
+    public void backupAssets(String[] assets, String tmpDir, String outputDir, String videoFileName) {
+        try {
+            this.setup(tmpDir);
             this.login();
 
             List<String> filePathList = new ArrayList<>();
@@ -165,27 +199,36 @@ public class PCloudBackup {
                     if (Files.isDirectory(assetPath)) folderPathList.add(assetPath.toString());
                     else filePathList.add(assetPath.toString());
                 } else {
-                    System.out.println("File/Folder does not exist: " + assetPath);
+                    logger.warn("File/Folder does not exist: {}", assetPath);
                 }
             }
-            fileUpload(Duration.ofSeconds(50), filePathList);
+            if (!filePathList.isEmpty()) fileUpload(Duration.ofSeconds(50), filePathList);
             folderPathList.forEach(folderPath -> folderUpload(Duration.ofSeconds(50), folderPath));
-            uploadChecker(Duration.ofMinutes(5), cntofAssets);
+            uploadChecker(Duration.ofMinutes(20), cntofAssets);
 
             // To get images of the last frame better
-            Thread.sleep(1000);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                logger.warn("Timer threw an exception. Ignored");
+            }
+
             devTools.send(Page.stopScreencast());
             devTools.clearListeners();
-            devTools.disconnectSession();
-            devTools.close();
 
-            if (Utils.generateVideoFromFrames(frameNamePrefix, videoFileName))
-                System.out.println("Successfully generated video from frames");
+            if (Utils.generateVideoFromFrames(frameNamePrefix, tmpDir, outputDir, videoFileName))
+                logger.info("Successfully generated video from frames: {}/{}", outputDir, videoFileName);
             else
-                System.out.println("Something went wrong! Unable to generate video from frames");
+                logger.error("Something went wrong! Process generating the frames ended with status code other than 0");
         } finally {
-            this.webDriver.close();
-            Utils.deleteFrames(seqNum);
+            try{
+                devTools.send(Page.stopScreencast());
+                devTools.clearListeners();
+            } catch(Exception e) {
+                logger.info("Event listeners might be already closed");
+            }
+            this.cleanUp();
+            Utils.deleteFrames(frameNamePrefix, tmpDir, seqNum);
         }
     }
 }
--- src/main/java/io/ashutosh/Utils.java
+++ src/main/java/io/ashutosh/Utils.java
@@ -1,6 +1,8 @@
 package io.ashutosh;
 
 import org.openqa.selenium.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,11 +11,15 @@ import java.nio.file.attribute.FileTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.BiPredicate;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class Utils {
+    private static final Logger logger = LoggerFactory.getLogger(Utils.class);
+
     public static boolean fileTimeComparator(FileTime first, FileTime second) {
         ZonedDateTime zdt1 = ZonedDateTime.ofInstant(first.toInstant(), ZoneId.of("Asia/Kolkata")).truncatedTo(ChronoUnit.SECONDS);
         ZonedDateTime zdt2 = ZonedDateTime.ofInstant(second.toInstant(), ZoneId.of("Asia/Kolkata")).truncatedTo(ChronoUnit.SECONDS);
@@ -48,30 +54,74 @@ public class Utils {
         };
     }
 
-    public static boolean generateVideoFromFrames(String frameNameSuffix, String videoFileName) throws IOException, InterruptedException {
+    public static boolean generateVideoFromFrames(String frameNamePrefix, String tmpDir, String outputDir, String videoFileName) {
         ProcessBuilder processBuilder = new ProcessBuilder(
                 "ffmpeg",
                 "-y",
                 "-loglevel", "quiet",
-                "-framerate", "15",
-                "-i", frameNameSuffix + "%d.png",
+                "-framerate", "30",
+                "-i", tmpDir + "/" + frameNamePrefix + "%d.png",
                 "-c:v", "libx264",
                 "-vf", "pad=ceil(iw/2)*2:ceil(ih/2)*2",
                 "-pix_fmt", "yuv420p",
-                videoFileName);
+                outputDir + "/" + videoFileName);
         processBuilder.redirectErrorStream(true);
-
-        Process process = processBuilder.start();
-        return process.waitFor() == 0;
+        try {
+            Process process = processBuilder.start();
+            return process.waitFor() == 0;
+        } catch (IOException e) {
+            logger.error("Something went wrong while starting the process to generate video from frames", e);
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            logger.error("Something went wrong while waiting for the video generation process to complete", e);
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void deleteFrames(Long maxFrameCount) {
+    public static void deleteFrames(String frameNamePrefix, String tmpDir, Long maxFrameCount) {
         LongStream.range(0, maxFrameCount).forEach(frameNum -> {
             try {
-                Files.deleteIfExists(Paths.get("").toAbsolutePath().resolve("frame-" + frameNum + ".png"));
+                Files.deleteIfExists(Paths.get(tmpDir).toAbsolutePath().resolve(frameNamePrefix + frameNum + ".png"));
             } catch (IOException e) {
+                logger.error("Enable to delete frames", e);
                 throw new RuntimeException(e);
             }
         });
     }
+
+    public static String messageTextBuilder(List<String> copiedFiles, List<String> failedPaths, boolean isFailed) {
+        StringBuilder text = new StringBuilder();
+
+        if (isFailed) text.append("<b>Backup Failed</b> ❌\n");
+        else if (!failedPaths.isEmpty()) text.append("<b>Backup Partially Successful</b> ✅\n");
+        else text.append("<b>Backup Successful</b> ✅\n");
+
+        // For failed:
+        //          1. List of files copied if present or nothing
+        //          2. Failed Paths
+        // For partial successful:
+        //          1. List of files copied if present or all files are up to date
+        //          2. Failed Paths
+        // For successful:
+        //          1. List of files copied if present or all files are up to date
+
+        if (!copiedFiles.isEmpty()) {
+            text.append("\n");
+            text.append("<b><i>List of files that were backed up in Hard Disk:</i></b>\n");
+            IntStream.range(0, copiedFiles.size()).forEach(idx -> text.append(idx + 1).append(". ").append(copiedFiles.get(idx)).append("\n"));
+        }
+
+        if (copiedFiles.isEmpty() && !isFailed) {
+            text.append("\n");
+            text.append("<b><i>All files are already up to date!</i></b>\n");
+        }
+
+        if (!failedPaths.isEmpty()) {
+            text.append("\n");
+            text.append("<b><i>List of paths that require attention:</i></b>\n");
+            IntStream.range(0, failedPaths.size()).forEach(idx -> text.append(idx + 1).append(". ").append(failedPaths.get(idx)).append("\n"));
+        }
+        return text.toString();
+    }
+
 }
